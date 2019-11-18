ce47e91de5793d45f6b8cb192080de5b2729539a
Ana-ishSodhi <57884711+Ana-ishSodhi@users.noreply.github.com>
2019-11-18 19:47:10 +1300

    Add files via upload

---
+++ Assignment 5/COMP102-2019T2-Assig5/ImageRenderer/RunImageRenderer.java
@@ -0,0 +1,19 @@
+// This program is copyright VUW.
+// You are granted permission to use it to construct your answer to a COMP102 assignment.
+// You may not distribute it in any other way without permission.
+
+/* Code for COMP102 - 2019T2, Assignment 5
+ * Name:
+ * Username:
+ * ID:
+ */
+
+import ecs100.*;
+
+/** RunImageRenderer   */
+public class RunImageRenderer{
+    public static void main(String[] args){
+        ImageRenderer ir = new ImageRenderer();
+        ir.setupGUI();
+    }
+}
